cd47669dc95abda6a7834acd747059e2c0bc61ca
joselevelsup <joselevelsup@gmail.com>
2023-01-25 17:45:39 -0500

    added more to http server

--- node-api-examples/cli-with-commander.js
+++ node-api-examples/cli-with-commander.js
@@ -2,11 +2,20 @@ const { program } = require("commander");
 
 program
   .option("-d, --debug", "output extra debugging")
-  .option("-s, --small", "small pizza size")
-  .option("-p, --pizza-type <type>", "flavour of pizza");
+  .option("-s, --size <size>", "Size of pizza", "small")
+  .option("-p, --pizza-type <type>", "flavour of pizza", "cheese");
 
 program.parse(process.argv);
 
 const options = program.opts();
 
 console.log(options);
+
+console.log(`Your pizza is ${options.pizzaType} and the size is ${options.size}`);
+
+
+// if(options.small){
+//   console.log(`Your pizza is ${options.pizzaType} and the size is small`);
+// } else {
+//   console.log(`Your pizza is ${options.pizzaType} and the size is large`);
+// }
